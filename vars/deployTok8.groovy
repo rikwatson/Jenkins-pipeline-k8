2f39fddcc307617fc17df46702b0cb03fdb6dbab
Gautam Sidhu <gautam.sidhu@ASLEA-WL-25151.kingsway.asos.com>
2019-03-06 10:37:22 +0000

    Adding validation

--- vars/deployTok8.groovy
+++ vars/deployTok8.groovy
@@ -44,6 +44,62 @@ def call(body) {
                     sh "${pipelineParams.kubectlPath} --kubeconfig='$KUBEFILE' apply -f deployments/${pipelineParams.envStage}/deployment.yaml --alsologtostderr=true"
                 }
             }
+
+            //Wait for a while for pods to spin up
+            stage('Wait for deployment to finish') {
+                sh "echo varifying deployment on '${pipelineParams.env_stage}' cluster, image ${pipelineParams.image_name}:${pipelineParams.image_tag}"
+                echo "Delaying for ${pipelineParams.wait_before_validate} secs..."
+                if (pipelineParams.wait_before_validate.isInteger()) {
+                    int wait_before_validate = pipelineParams.wait_before_validate as Integer
+                    sleep(wait_before_validate)
+                }
+            }
+
+            stage ('Verify deployment ') {
+                expectedStatus = "deployment \"${pipelineParams.app_name}-deployment-${pipelineParams.git_hash}\" successfully rolled out"
+                withCredentials([file(credentialsId: "${pipelineParams.kubeConfigCredId}", variable: 'KUBEFILE')]) {
+                    sh "${pipelineParams.kubectlPath} --kubeconfig='$KUBEFILE' get deployment ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash}"
+                    def rolloutStatus = sh (
+                            script: "${pipelineParams.kubectlPath} --kubeconfig='$KUBEFILE' rollout status deployment ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash}",
+                            returnStdout: true
+                        ).trim()
+                        if (expectedStatus == rolloutStatus) {
+                            sh "echo depolyment successfully rolled out."
+                        }
+                        else {
+                            print rolloutStatus
+                            currentBuild.result = 'FAILURE'
+                            return
+                        }
+                    sh "${pipelineParams.kubectlPath} --kubeconfig='$KUBEFILE' get deployment ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash} > deploymentStatus"
+
+                    def desiredPodStatus = sh (
+                        script: "grep ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash} deploymentStatus | awk '{print \$2}'",
+                        returnStdout: true
+                    ).trim()
+                    def currentPodStatus = sh (
+                        script: "grep ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash} deploymentStatus | awk '{print \$3}'",
+                        returnStdout: true
+                    ).trim()
+                    def uptodatePodStatus = sh (
+                        script: "grep ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash} deploymentStatus | awk '{print \$4}'",
+                        returnStdout: true
+                    ).trim()
+                    def availablePodStatus = sh (
+                        script: "grep ${pipelineParams.app_name}-deployment-${pipelineParams.git_hash} deploymentStatus | awk '{print \$5}'",
+                        returnStdout: true
+                    ).trim()
+
+                    if (desiredPodStatus == currentPodStatus == uptodatePodStatus == availablePodStatus) {
+                        sh "echo All Pods successfully deployed!"
+                    }
+                    else {
+                        print availablePodStatus
+                        currentBuild.result = 'FAILURE'
+                        return
+                    }
+                }
+            }
         } catch (err) {
             currentBuild.result = 'FAILED'
             throw err
